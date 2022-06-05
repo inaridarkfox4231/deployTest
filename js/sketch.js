2571206c9522ae03173c67b7abc10d4fc37d8846
inaridarkfox4231 <sea_gull6389-4m@ka.tnc.ne.jp>
2022-06-05 10:27:02 +0900

    Deployed 2ffe8df with MkDocs version: 1.3.0

--- js/sketch.js
+++ js/sketch.js
@@ -1,1 +1,21 @@
 // p5jsのスケッチを作る
+let mySketch = p => {
+    let bg;
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight);
+        bg = p.createGraphics(p.width, p.height);
+        bg.colorMode(p.HSB, 100);
+        bg.noStroke();
+        for(let i = 0; i < 100; i++){
+            bg.fill(5, i/2, 100);
+            bg.rect(0, bg.height*i/100, bg.width, bg.height/100);
+        }
+    }
+    p.draw = () => {
+        p.image(bg, 0, 0);
+    }
+};
+
+if(document.getElementById("sketchArea0")){
+    new p5(mySketch, "sketchArea0");
+}
